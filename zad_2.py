6c6fba57fb929aac46bc6bf1946351687fce1026
DariuszKusnierz <dariusz.kusnierz@edu.uekat.pl>
2022-10-24 22:54:43 +0200

    dokończone zadanie 2

--- zad_2.py
+++ zad_2.py
@@ -1,5 +1,3 @@
-from dbm.ndbm import library
-
 
 class Libraby:
     def __init__(self,city,street,zip_code,open_hours,phone) -> None:
@@ -52,7 +50,7 @@ class Order:
         pass
 
     def __str__(self) -> str:
-        return f'Zamówienie realizowane przez {self.employee} dla studenta {self.student} obejmuje następujące książki: {self.books}. Data zamówienia: {self.order_date}.'
+        return f'Zamówienie realizowane przez {self.employee} dla studenta {self.student} obejmuje następujące książki: {self.books} Data zamówienia: {self.order_date}.'
         pass
 
 class Student:
@@ -68,6 +66,10 @@ class Student:
         mean /= len(self.marks)
         return mean > 50
 
+    def __str__(self) -> str:
+        return f'{self.name} z ocenami: {self.marks}'
+        pass
+
 library_1 = Libraby("Katowice","Kościuszki 12", "11-111", "12-18", 123345567)
 library_2 = Libraby("Gliwice","Sienkiewicza 5", "12-121", "10-17", 923345561)
 
@@ -82,3 +84,11 @@ employee_2 = Employee("Ewa", "Wolna", "21.08.2021", "01.05.1987", "Gliwice","Sie
 employee_3 = Employee("Arkadiusz", "Lipski", "09.05.2020", "21.09.2001", "Katowice","Warszawska 3", "11-131", 632346567)
 
 student_1 = Student("Marek", [40,11])
+student_2 = Student("Ania", [19,70,50])
+student_3 = Student("Kasia", [11,9,81])
+
+order_1 = Order(employee_1,student_1,book_1, "21.09.2022")
+order_2 = Order(employee_2,student_2,book_2, "11.05.2022")
+
+print(order_1)
+print(order_2)
