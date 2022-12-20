5f0341ae91032c6f762ec29ddcbeaf61561f3648
DariuszKusnierz <dariusz.kusnierz@edu.uekat.pl>
2022-12-20 16:00:46 +0100

    dodano przycisk powrotu

--- findPerson.py
+++ findPerson.py
@@ -1,39 +1,21 @@
 import imutils
 import cv2
 
-# Initializing the HOG person 
-hog = cv2.HOGDescriptor() 
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector()) 
-#hog.setSVMDetector(cv2.HOGDescriptor_getDaimlerPeopleDetector())
-
-#hog = cv2.HOGDescriptor('WinSize',[48,48*2], 'NLevels',13, 'GammaCorrection',True)
-#hog.SvmDetector = 'DaimlerPeopleDetector'
-#opts = {'HitThreshold',1.4, 'WinStride',[8,8], 'Padding',[0,0], 'Scale',1.05, 'FinalThreshold',8}
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 
 def CheckImage(userImg):
-    # Reading the Image 
-    image = cv2.imread(userImg) 
-    #con_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Resizing the Image 
-    image = imutils.resize(image, width=min(600, image.shape[1])) 
-   
-    # Detecting all humans 
+    image = cv2.imread(userImg)
+    image = imutils.resize(image, width=min(600, image.shape[1]))
     (humans, _) = hog.detectMultiScale(image,  winStride=(5, 5), padding=(3, 3), scale=1.08)
-    # getting no. of human detected
-    print('Human Detected : ', len(humans))
-   
-    # Drawing the rectangle regions
     for (x, y, w, h) in humans: 
         cv2.rectangle(image, (x, y),  
                     (x + w, y + h),  
                     (0, 0, 255), 2) 
-  
-    # Displaying the output Image 
-    #return image
+
     cv2.imwrite("static/IMG/img.jpg", image)
-    #cv2.imshow("Image", image) 
-    #cv2.waitKey(0)
+
 
 
 #def SaveImgInMemory(image):
